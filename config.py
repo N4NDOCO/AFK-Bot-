6836db6c0781d5e3d102f382982a3b83c4a85391
N4NDOCO <andersonsilva978x@gmail.com>
2026-01-29 15:27:12 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
 
-# Token do bot (use variável de ambiente no Koyeb)
-TOKEN = os.environ.get("TOKEN")  
-
-# IDs do servidor e do canal de AFK
-GUILD_ID = 1465477542919016625
-AFK_CHANNEL_ID = 1466487369195720777
+TOKEN = os.environ.get("TOKEN")  # variável de ambiente
+GUILD_ID = 1465477542919016625   # ID do seu servidor
+AFK_CHANNEL_ID = 1466487369195720777  # canal ⏳┃afk-status
