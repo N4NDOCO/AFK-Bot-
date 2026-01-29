6bd29270910f32988bd6b5fd24df052a0b9f5470
N4NDOCO <andersonsilva978x@gmail.com>
2026-01-29 14:52:41 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")            # variável de ambiente no Koyeb
+GUILD_ID = 1465477542919016625             # ID do seu servidor
+AFK_CHANNEL_ID = 1466487369195720777      # ID do canal ⏳ | afk-status
